3f89917194df27b79dfcc5c942b22f92d7e17290
Juliana Freitas <julianachristello@gmail.com>
2021-04-09 21:16:28 -0300

    atualizando

--- script.js
+++ script.js
@@ -1,14 +1,12 @@
 let boxcards = document.querySelector(".boxcards");
-
-let cardsnumber = prompt("Qual o número de cartas? Número par de 4 a 14");
-
+let cardsnumber = parseInt(prompt("Qual o número de cartas? Número par de 4 a 14"));
 
 while (cardsnumber % 2 != 0 || cardsnumber > 14 || cardsnumber < 4) {
-    cardsnumber = prompt("Qual o número de cartas? Somente número par entre 4 a 14");
+    cardsnumber = parseInt(prompt("Qual o número de cartas? Somente número par entre 4 a 14"));
 }
 
 let imgs = []
-imgs[0] = "imgs/bobrossparrot"
+imgs[0] = "imgs/bobrossparrot.gif"
 imgs[1] = "imgs/explodyparrot.gif"
 imgs[3] = "imgs/fiestaparrot.gif"
 imgs[4] = "imgs/metalparrot.gif"
@@ -17,21 +15,21 @@ imgs[6] = "imgs/tripletsparrot.gif"
 imgs[7] = "imgs/tripletsparrot.gif";
 
 
+
+
 let foldedImgs = [];
 let arraycards = [];
-
 
 for (let i = 0; i < cardsnumber / 2; i++) {
     foldedImgs.push(imgs[i]);
     foldedImgs.push(imgs[i]);
 }
-
   foldedImgs.sort(randomSort);
 
 for (let i = 0; i < cardsnumber/2; i++) {
-    arraycards.push(`<div class="card"> 
+    arraycards.push(`<li class="card"> 
     <img onclick="changeimg(this)" id="${foldedImgs[i]}" src="imgs/front.png" 
-    alt="papagaio da frente da carta"> </div>`);
+    alt="papagaio da frente da carta"> </li>`);
 
     arraycards.push(`<div class="card"> 
     <img onclick="changeimg(this)" id="${foldedImgs[i]}" src="imgs/front.png" 
@@ -39,20 +37,21 @@ for (let i = 0; i < cardsnumber/2; i++) {
 }
 
 arraycards.sort(randomSort);
-
 console.log(arraycards);
+
+for (let i = 0; i < arraycards.lenght; i++)  {
+    boxcards.innerHTML += arraycards[i];
+}
 
 function changeimg(imagemcard) {
     imagemcard.setAttribute('src', imagemcard.id);
 }
-
 
 function newboxcards() {
     boxcards.innerHTML += `${boxcards.innerHTML}`;
 }
 
 newboxcards();
-
 
 function randomSort() {
     return Math.random() - 0.5;
